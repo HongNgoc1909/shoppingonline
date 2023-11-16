9113a85d4357f454fb38c2d3b0b3e65cc591954f
HongNgoc1909 <127505976+HongNgoc1909@users.noreply.github.com>
2023-11-16 14:31:44 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.8v6lngg.mongodb.net',
+    DB_USER: 'nthongngoc199203',
+    DB_PASS: 'Nthongngoc19092003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'nvh',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'nthongngoc199203@gmail.com', // gmail service
+    EMAIL_PASS: 'fzua dbse wdjj fykv'
+  };
+  module.exports = MyConstants;
